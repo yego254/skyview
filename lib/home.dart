1f789c0eb06546ef38715e9b31594a0d318e89a6
unknown <sam18ke@gmail.com>
2022-12-08 14:32:05 +0300

    new file

---
+++ lib/home.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:skyv/cookie_page.dart';
+import 'package:skyv/bottom_bar.dart';
+
+import 'Beverage_page.dart';
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor:Colors.amber[700],
+        elevation: 0.0,
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.fastfood,size: 40.0, color: Colors.white),
+          onPressed: () {},
+        ),
+        title: Text('SkyView Deliveries ',
+            style: TextStyle(
+                fontFamily: 'Varela',
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white)),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.notifications_none, color: Colors.white),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: ListView(
+        padding: EdgeInsets.only(left: 20.0),
+        children: <Widget>[
+          SizedBox(height: 15.0),
+          Text('Categories',
+              style: TextStyle(
+                  fontFamily: 'Varela',
+                  fontSize: 42.0,
+                  fontWeight: FontWeight.bold)),
+          SizedBox(height: 15.0),
+          TabBar(
+              controller: _tabController,
+              indicatorColor: Colors.transparent,
+              labelColor: Color(0xFFC88D67),
+              isScrollable: true,
+              labelPadding: EdgeInsets.only(right: 45.0),
+              unselectedLabelColor: Color(0xFFCDCDCD),
+              tabs: [
+                Tab(
+                  child: Text('Snacks',
+                      style: TextStyle(
+                        fontFamily: 'Varela',
+                        fontSize: 21.0,
+                      )),
+                ),
+                Tab(
+                  child: Text('Beverages',
+                      style: TextStyle(
+                        fontFamily: 'Varela',
+                        fontSize: 21.0,
+                      )),
+                ),
+                Tab(
+                  child: Text('Main Dishes',
+                      style: TextStyle(
+                        fontFamily: 'Varela',
+                        fontSize: 21.0,
+                      )),
+                )
+              ]),
+          Container(
+              height: MediaQuery.of(context).size.height - 50.0,
+              width: double.infinity,
+              child: TabBarView(
+
+                  controller: _tabController,
+                  children: [
+                    CookiePage(),
+                    BPage(),
+                    CookiePage(),
+                  ]
+              )
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+
+
+      },
+        backgroundColor: Color(0xFFF17532),
+        child: Icon(Icons.fastfood),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomBar(),
+    );
+  }
+}
